eb4b0a391173fa320446f97de008f3a134aa1292
Elijah-Rae Earl <elijahrae@outlook.com>
2023-03-03 13:58:57 +0000

    quick trim serverless-related files

--- aws/dynamo/DynamoClient.ts
+++
@@ -1,6 +0,0 @@
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-
-const REGION = "eu-west-1";
-
-export const DynamoClient = DynamoDBDocument.from(new DynamoDB({ region: REGION }));
